41f78e5c96d83cf9ca9bac532695c79c1c5f0691
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2024-07-14 08:57:31 +0700

    add kontak portfolio tentang

---
+++ app/kontak/page.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
+
+export const metadata = {
+    openGraph: {
+        title: 'Kontak - Arrosyid Al Ayubi',
+        description: 'Arrosyid Al Ayubi Personal Website',
+        url: 'https://www.arrosyid.my.id/kontak',
+        siteName: 'ArrosyidBlog',
+        images: [
+            {
+                url: 'https://www.arrosyid.my.id/og.png', // Must be an absolute URL
+                width: 800,
+                height: 600,
+            },
+            {
+                url: 'https://www.arrosyid.my.id/og-alt.png', // Must be an absolute URL
+                width: 1800,
+                height: 1600,
+                alt: 'OpenGraph Image',
+            },
+        ],
+        locale: 'id_ID',
+        type: 'website',
+    },
+}
+export default async function Kontak() {
+    return (
+        <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+            <h1>Kontak</h1>
+        </div>
+    )
+}
